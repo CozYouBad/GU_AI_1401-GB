985c1daa6b47d6c3248599ea6da20b06f1d5eae6
CozYouBad <dmorozevich@gmail.com>
2021-05-13 18:33:40 +0200

    Урок 3 задание 2

---
+++ l3/dz3.2.py
@@ -0,0 +1,4 @@
+def my_func(name, surname, birthdate, city, email, phone):
+    print(name, surname, birthdate, city, email, phone)
+    return 0
+my_func(name='John', surname='Johns,', birthdate= '29 February 1991,', city='New York,', email='johnjohns@gmail.com,', phone='+123455667' )
